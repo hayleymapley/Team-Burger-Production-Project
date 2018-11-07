dbb9cca3e5a13a3e671c67acbc1c15763bfeb154
hayleymapley <hmapley@gmail.com>
2018-11-07 13:56:10 +1300

    Changed to UI class to extend Application (JavaFX)

--- BurgerProduction/src/UI/JavaFXUI.java
+++ BurgerProduction/src/UI/JavaFXUI.java
@@ -2,14 +2,37 @@ package UI;
 
 import java.util.List;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import productiionLineDataClasses.Ingredient;
 import productiionLineDataClasses.Order;
 
-public class JavaFXUI {
+public class JavaFXUI extends Application {
 	
 	List<Order> orders;
 	
 	List<Ingredient> ingredients;
+	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 	
 	public boolean viewOrder(Order order) {
 		
