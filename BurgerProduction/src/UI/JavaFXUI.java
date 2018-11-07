1abc21a8d267c81c16703cb88e129e25f2f086ca
hayleymapley <hmapley@gmail.com>
2018-11-07 14:11:35 +1300

    Adding css file

--- BurgerProduction/src/UI/JavaFXUI.java
+++ BurgerProduction/src/UI/JavaFXUI.java
@@ -18,12 +18,17 @@ public class JavaFXUI extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
 		// TODO Auto-generated method stub
 		try {
 			BorderPane root = new BorderPane();
+			
 			Scene scene = new Scene(root,400,400);
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			primaryStage.setScene(scene);
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
