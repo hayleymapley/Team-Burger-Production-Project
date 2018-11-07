1b027e09cccb9b2569fa57fcdc800fb65f016372
embracethemirth <40256547+embracethemirth@users.noreply.github.com>
2018-11-08 12:00:16 +1300

    Updated Customer class

--- BurgerProduction/src/productiionLineDataClasses/Customer.java
+++ BurgerProduction/src/productiionLineDataClasses/Customer.java
@@ -1,5 +1,34 @@
 package productiionLineDataClasses;
 
 public class Customer {
+	private int customerID;
+	private String customerName;
+	private String customerEmail;
+	
+	public Customer(int customerID, String customerName, String customerEmail) {
+		this.customerID = customerID;
+		this.customerName = customerName;
+		this.customerEmail = customerEmail;
+	}
+	
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
 
 }
