5eb1abf7919b2dc76a0e08fcdb65b0423bf0858f
Chadz <karannchat@ecs.vuw.ac.nz>
2018-11-08 18:16:02 +1300

    orderclass finished

--- BurgerProduction/src/handlers/OrderHandler.java
+++ BurgerProduction/src/handlers/OrderHandler.java
@@ -4,22 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
 
-import UI.JavaFXUI;
+
 import productiionLineDataClasses.Burger;
 import productiionLineDataClasses.Customer;
+import productiionLineDataClasses.Ingredient;
 import productiionLineDataClasses.Order;
 
 public class OrderHandler {
 	private String databaseUser = "karannchat";
 	private String databaseUserPass = "123";
-	private HashMap<Integer, Order> currentOrders;
+
 
 	// global method to connect the jdbc
 	private Connection connect() throws SQLException {
@@ -29,7 +31,6 @@ public class OrderHandler {
 
 			String url = "jdbc:postgresql://db.ecs.vuw.ac.nz/" + databaseUser + "_jdbc";
 			connection = DriverManager.getConnection(url, databaseUser, databaseUserPass);
-			Statement s = connection.createStatement();
 
 		} catch (ClassNotFoundException e) {
 			System.out.println(e.getMessage());
@@ -39,71 +40,131 @@ public class OrderHandler {
 
 	public List<Order> retrieveOrdersFromDB() throws SQLException { 
 		// goes to DB and retrieves orders for java FX GUI  list view
-		int order_id = 0;
-
+		Timestamp orderTS = null;
+		int orderID = 0;
+		boolean orderComplete = false;
 		List<Order> orderNotCompleteList = new ArrayList<>();
+		int customerID = 0;
+
 		Connection conn = connect();
 		Statement stmt = conn.createStatement();
 		// Searching not completed orderdId from order table
-		String orderSql = "SELECT order_id FROM  orders WHERE COMPLETED= 'f' ";
+		String orderSql = "SELECT * FROM  orders WHERE COMPLETED = 'f' ";
 		ResultSet rs = stmt.executeQuery(orderSql);
 		while (rs.next()) {
-			order_id = rs.getInt("order_id");
-			System.out.println("order_id for uncomplete order is: " + order_id);
 			
-		}
+			orderID = rs.getInt("order_id");
 
+			orderTS = rs.getTimestamp("timestamp");
+
+			orderComplete = rs.getBoolean("completed");
+			
+			customerID = findCustomerID(orderID);
+			
+			Order newOrder = new Order(orderComplete, orderTS, orderID, findCustomer(customerID),findBurgerIngredients(orderID));
+			
+			orderNotCompleteList.add(newOrder);
+
+		}
+		
 		rs.close();
 		stmt.close();
 		
+		System.out.println(orderNotCompleteList.size());
 		
+		System.out.println(orderNotCompleteList.get(0).getCustomer().getCustomerName());
+		
+		System.out.println(orderNotCompleteList.get(1).getCustomer().getCustomerName());
 
 		return orderNotCompleteList;
 	}
 
-	public Order findBurgerIngredients(int orderID) throws SQLException {
-		//finds burger details and adds them into a burger object
-		int bunLettuceAmount = 0, bunStandardAmount = 0, vegeLettuceAmount = 0,
-				vegeTomatoAmount = 0, vegeOnionAmount = 0, vegePicklesAmount = 0, 
-				vegeBeetrootAmount = 0, cheeseCheddarAmount = 0, cheeseVeganAmount = 0,
-				pattyBeefAmount = 0, pattyChickenAmount = 0, pattyTofuAmount = 0, 
-				sauceTomatoAmount = 0, sauceChilliAmount = 0, sauceAioliAmount = 0;
+	public List<Burger> findBurgerIngredients(int orderID) throws SQLException {
 
-		String bunLettuceName = null, bunStandardName = null, vegeLettuceName = null,
-				vegeTomatoName = null, vegeOnionName = null, vegePicklesName = null,
-				vegeBeetrootName = null, cheeseCheddarName = null, cheeseVeganName = null,
-				pattyBeefName = null, pattyChickenName = null, pattyTofuName = null, 
-				sauceTomatoName = null, sauceChilliName = null, sauceAioliName = null;
+		List <Ingredient> ingredientsInBurgerList = new ArrayList<>();
+		List <Burger> currentBurgersList = new ArrayList<>();
+		int ingredientQuantity = 0;
+		String ingredientName = " ";
 
 		Connection conn = connect();
 		Statement stmt = conn.createStatement();
 		String burgerSql = "SELECT * FROM  burger_ingredients WHERE order_id = "+ orderID;
-		ResultSet burgerResult = stmt.executeQuery(burgerSql);
 		Statement burgerStatement= conn.createStatement();
+		ResultSet burgerResult = stmt.executeQuery(burgerSql);
+		ResultSetMetaData rsmd = burgerResult.getMetaData();
 		while (burgerResult.next()) {
-			bunLettuceAmount = 	burgerResult.getInt("Bun_Lettuce");
-			bunStandardAmount = burgerResult.getInt("Bun_Standard");
-			vegeLettuceAmount = burgerResult.getInt("Vege_Lettuce");
-			vegeTomatoAmount = burgerResult.getInt("Vege_Tomato");
-			vegeOnionAmount = burgerResult.getInt("Vege_Onion");
-			vegePicklesAmount = burgerResult.getInt("Vege_Pickles");
-			vegeBeetrootAmount = burgerResult.getInt("Vege_Beetroot");
-			cheeseCheddarAmount = burgerResult.getInt("Cheese_Cheddar");
-			cheeseVeganAmount = burgerResult.getInt("Cheese_Vegan");
-			pattyBeefAmount = burgerResult.getInt("Patty_Beef");
-			pattyChickenAmount = burgerResult.getInt("Patty_Beef");
-			pattyTofuAmount = burgerResult.getInt("Patty_Tofu");
-			sauceTomatoAmount = burgerResult.getInt("Sauce_Tomato");
-			sauceChilliAmount = burgerResult.getInt("Sauce_Chilli");
-			sauceAioliAmount = burgerResult.getInt("Sauce_Aioli");
+
+			for(int i = 2; i <= 17; i++) {
+				if(burgerResult.getInt(i) != 0) {
+
+					ingredientQuantity = burgerResult.getInt(i);
+					ingredientName = rsmd.getColumnName(i);
+					Ingredient newIngredient = new Ingredient(ingredientName, ingredientQuantity);
+					ingredientsInBurgerList.add(newIngredient);
+				}
+
+			}
+
+			Burger newBurger = new Burger(ingredientsInBurgerList);
+			currentBurgersList.add(newBurger);
 
 		}
+
 		burgerResult.close();
 		burgerStatement.close();
-		return null;
+
+		return currentBurgersList;
 	}
 
-	public boolean setOrderToComplete(Order order) throws SQLException {
+	public int findCustomerID(int orderID) throws SQLException{ 
+		int customerID = 0;
+
+		Connection connection = connect();
+		Statement statement = connection.createStatement();
+		String customerSql = "SELECT customer_id FROM  orders WHERE order_id = "+ orderID;
+		Statement customerStatement= connection.createStatement();
+		ResultSet customerResult = statement.executeQuery(customerSql);
+
+		while(customerResult.next()) {
+			customerID = customerResult.getInt("customer_id");
+		}
+
+		customerResult.close();
+		customerStatement.close();
+
+		//find customerID, customer name and email then create Customer object to be returned
+
+		return customerID;
+	}
+
+	public Customer findCustomer(int customerID) throws SQLException{ 
+		String customerName = null;
+		String customerEmail = null;
+
+		Connection connection = connect();
+		Statement statement = connection.createStatement();
+		String customerSql = "SELECT * FROM  customers WHERE customer_id = "+ customerID;
+		Statement customerStatement= connection.createStatement();
+		ResultSet customerResult = statement.executeQuery(customerSql);
+
+		while(customerResult.next()) {
+			customerName = customerResult.getString("name");
+			customerEmail = customerResult.getString("email");
+		}
+
+		Customer newCustomer = new Customer(customerID, customerName, customerEmail);
+
+		customerResult.close();
+		customerStatement.close();
+
+		return newCustomer;
+	}
+
+	//find customerID, customer name and email then create Customer object to be returned
+
+
+
+	public void setOrderToComplete(Order order) throws SQLException {
 		// method is called upon when "complete order" button is pressed in UI
 
 		// print out current status of order
@@ -115,11 +176,9 @@ public class OrderHandler {
 		Connection conn = connect();
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.executeUpdate();
-		
+
 		// print the status after status change
 		System.out.println("Check Order status once update has been completed: " + checkOrderStatus(order));
-		
-		return false;
 	}
 
 	public boolean checkOrderStatus(Order order) throws SQLException {
@@ -141,15 +200,11 @@ public class OrderHandler {
 
 	}
 
-	public static void main(String[] args) throws SQLException {
+	/*public static void main(String[] args) throws SQLException {
 		OrderHandler o = new OrderHandler();
-		List<Burger> burgers = new ArrayList();
-
-		Customer cx = new Customer(4, "Jeramy", "jeramy@gmail.com");
-
-		//Order orderOne = new Order(4, cx, burgers);
 		o.retrieveOrdersFromDB();
-		// o.setOrderToComplete(orderOne);
-	}
+
+		
+	}*/
 
 }
