1ad6e685518d559a915f0dd7eafc6961d287566f
hayleymapley <hmapley@gmail.com>
2018-11-12 15:46:22 +1300

    Added create order method

--- BurgerProduction/WebApp/orderConfirm.php
+++ BurgerProduction/WebApp/orderConfirm.php
@@ -10,12 +10,49 @@ function createCustomer($name, $email) {
     or die("Can't connect to database".pg_last_error());
     
     $sql = pg_query($dbconn3,"INSERT INTO customers (name,email) VALUES('$name', '$email')");
+    
+    $query = "SELECT customer_ID FROM customers where name = '$name' AND email = '$email'";
+    $result = pg_query($query);
+    if (!$result) {
+        echo "Problem with query " . $query . "<br/>";
+        echo pg_last_error();
+        exit();
+    }
+    $row = pg_fetch_assoc($result);
+    $customerID = $row['customer_id'];
+    $timestamp = date('Y-m-d G:i:s');
+    
+    echo "create customer method here";
+    
+    createOrder($customerID, $timestamp);
+    
 }
 
 if (isset($_POST['confirm'])) {
     createCustomer($name, $email);
 }
 
-header('Location: index.html');
+
+
+
+function createOrder($customerID, $timestamp) {
+    
+    $dbconn3 = pg_connect("host=db.ecs.vuw.ac.nz dbname=mapleyhayl_jdbc user=mapleyhayl password=pass123")
+    or die("Can't connect to database".pg_last_error());
+    
+    $sql = pg_query($dbconn3,"INSERT INTO orders (customer_id,completed,timestamp) VALUES('$customerID', false, '$timestamp')");
+    
+    $query = "SELECT order_id FROM orders WHERE customer_id = '$customerID' AND timestamp='$timestamp'";
+    $result = pg_query($query);
+    if (!$result) {
+        echo "Problem with query " . $query . "<br/>";
+        echo pg_last_error();
+        exit();
+    }
+    $row = pg_fetch_assoc($result);
+    $orderID = $row['order_id'];
+}
+
+// header('Location: testform2.php');
 
 ?>
