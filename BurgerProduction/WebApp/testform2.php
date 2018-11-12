b5456a38a32cff8b11dbc1cc95d2144b85b1d8de
faanannath <faanannath@gs1604.AD.ECS.VUW.AC.NZ>
2018-11-12 13:14:09 +1300

    Update testphp page

--- BurgerProduction/WebApp/testform2.php
+++ BurgerProduction/WebApp/testform2.php
@@ -15,7 +15,7 @@ $i = "hi";
 $arr = pg_fetch_all($result);
 foreach ($arr as &$value) {
 print_r($value);
-    }
+}
 
     //Add new customer row and print just ID
     //$sql = pg_query($dbconn3,"INSERT INTO customers (name,email) VALUES('john', 'john@example.com')");
@@ -34,12 +34,6 @@ print_r($value);
     echo "customerID =" . "$customerID <br/>";
 ?>
 
-
-
-
-
-
-
 <!DOCTYPE html>
 <html>
 <!--  <script type="text/javascript" src="main2.js"></script>-->
@@ -49,56 +43,43 @@ print_r($value);
 
 <p>Total Cost: $<span id="total"></span></p>
 <output id = "total1" name=total> test</output><br>
-
-Lettuce Bun: <input type="checkbox" id="Bun_Lettuce" onclick="myFunction(this.id)" value="5">
-<p id="Bun_Lettuce1" style="display:none">you selected a lettuce bun</p><br>
-
-standard bun: <input type="checkbox" id="Bun_Standard" onclick="myFunction(this.id)" value="5">
-<p id="Bun_Standard1" style="display:none">you selected a patty</p><br>
-
-veggie lettuce: <input type="checkbox" id="Vege_Lettuce" onclick="myFunction(this.id)" value="5">
-<p id="Vege_Lettuce1" style="display:none">you selected a patty</p><br>
-
-Vege tomato: <input type="checkbox" id="Vege_Tomato" onclick="myFunction(this.id)" value="5">
-<p id="Vege_Tomato1" style="display:none">you selected a patty</p><br>
-
-vege onion: <input type="checkbox" id="Vege_Onion" onclick="myFunction(this.id)" value="5">
-<p id="Vege_Onion1" style="display:none">you selected a patty</p><br>
-
-vege pickles: <input type="checkbox" id="Vege_Pickles" onclick="myFunction(this.id)" value="5">
-<p id="Vege_Pickles1" style="display:none">you selected a patty</p><br>
-
-vege beetroot: <input type="checkbox" id="Vege_Beetroot" onclick="myFunction(this.id)" value="5">
-<p id="Vege_Beetroot1" style="display:none">you selected a patty</p><br>
-
-cheese cheddar: <input type="checkbox" id="Cheese_Cheddar" onclick="myFunction(this.id)" value="5">
-<p id="Cheese_Cheddar1" style="display:none">you selected a patty</p><br>
-
-cheese vegan: <input type="checkbox" id="Cheese_Vegan" onclick="myFunction(this.id)" value="5">
-<p id="Cheese_Vegan1" style="display:none">you selected a patty</p><br>
-
-patty beef: <input type="checkbox" id="Patty_Beef" onclick="myFunction(this.id)" value="5">
-<p id="Patty_Beef1" style="display:none">you selected a patty</p><br>
-
-patty chicken: <input type="checkbox" id="Patty_Chicken" onclick="myFunction(this.id)" value="5">
-<p id="Patty_Chicken1" style="display:none">you selected a patty</p><br>
-
-patty tofu: <input type="checkbox" id="Patty_Tofu" onclick="myFunction(this.id)" value="5">
-<p id="Patty_Tofu1" style="display:none">you selected a patty</p><br>
-
-sauce tomato: <input type="checkbox" id="Sauce_Tomato" onclick="myFunction(this.id)" value="5">
-<p id="Sauce_Tomato1" style="display:none">you selected a patty</p><br>
-
-sauce chilli: <input type="checkbox" id="Sauce_Chilli" onclick="myFunction(this.id)" value="5">
-<p id="Sauce_Chilli1" style="display:none">you selected a patty</p><br>
-
-Sauce aioli: <input type="checkbox" id="Sauce_Aioli" onclick="myFunction(this.id)" value="5">
-<p id="Sauce_Aioli1" style="display:none">you selected a patty</p><br>
-
 <form action="">
+
+Lettuce Bun: <input type="checkbox" id="Bun_Lettuce"><br>
+
+Standard bun: <input type="checkbox" id="Bun_Standard"><br>
+
+veggie lettuce: <input type="number" id="Vege_Lettuce"><br>
+
+Vege tomato: <input type="number" id="Vege_Tomato"><br>
+
+vege onion: <input type="number" id="Vege_Onion"><br>
+
+vege pickles: <input type="number" id="Vege_Pickles"><br>
+
+vege beetroot: <input type="number" id="Vege_Beetroot"><br>
+
+cheese cheddar: <input type="number" id="Cheese_Cheddar"><br>
+
+cheese vegan: <input type="number" id="Cheese_Vegan"><br>
+
+patty beef: <input type="number" id="Patty_Beef"><br>
+
+patty chicken: <input type="number" id="Patty_Chicken"><br>
+
+patty tofu: <input type="number" id="Patty_Tofu"><br>
+
+sauce tomato: <input type="number" id="Sauce_Tomato"><br>
+
+sauce chilli: <input type="number" id="Sauce_Chilli"><br>
+
+Sauce aioli: <input type="number" id="Sauce_Aioli"><br>
+
+<br>
 <input type="text" id="name" required></input>
 <input type="text" id="email" required></input>
 <button type="submit">Confirm</button>
+
 </form>
 
 <output id= "total2" >test</output>
@@ -115,6 +96,7 @@ document.getElementById("total1").innerHTML = 5 + 6;
 
 var ingredientsArray = <?php echo json_encode($arr);?>;
 var i = 0;
+
 //for(i;i<ingredientsArray.length;i++){
 //	var current =  ingredientsArray[i];
 //	var name = current.name;
@@ -131,22 +113,31 @@ alert(ingredientsArray2.join('\n'));
 
 function myFunction(element) {
 	  var id = element;
-	  // Get the checkbox
-//	  var checkBox = document.getElementById("Bun_Lettuce");
-	  var checkBox = document.getElementById(id);
+	  // Get the number
+//	  var number = document.getElementById("Bun_Lettuce");
+	  //var number = document.getElementById(id);
+	  var quantity = document.getElementById(id);
 	  // Get the output text
 //	  var text = document.getElementById("Bun_Lettuce1");
 	  var text = document.getElementById(id + "1");
 	  
 	  var total = document.getElementById("total1");
 
-	  // If the checkbox is checked, display the output text
-	  if (checkBox.checked == true){
-	    text.style.display = "block";
-	    total.innerHTML = 1;
-	  } else {
-	    text.style.display = "none";
-	  }
+	  // If the number is checked, display the output text
+// 	  if (number.checked == true){
+// 	    text.style.display = "block";
+// 	    total.innerHTML = 1;
+// 	  } else {
+// 	    text.style.display = "none";
+// 	  }
+
+		//Deal with input type "number"<input type="number" name="quantity" min="1" max="5">
+// 	  if (quantity > 0){
+// 		    text.style.display = "block";
+// 		    total.innerHTML = 1;
+// 		  } else {
+// 		    text.style.display = "none";
+// 		  }
 	}
 </script>
 
