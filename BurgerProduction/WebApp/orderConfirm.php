9a9c79c71f1a7f1f1c633d7fd7f0b4817c75987c
hayleymapley <hmapley@gmail.com>
2018-11-12 16:27:46 +1300

    createBurger method completed

--- BurgerProduction/WebApp/orderConfirm.php
+++ BurgerProduction/WebApp/orderConfirm.php
@@ -5,15 +5,17 @@ error_reporting(E_ALL);
 $name = $_POST['name'];
 $email = $_POST['email'];
 
-function createCustomer($name, $email) {
-    $dbconn3 = pg_connect("host=db.ecs.vuw.ac.nz dbname=mapleyhayl_jdbc user=mapleyhayl password=pass123")
-    or die("Can't connect to database".pg_last_error());
+
+
+function createCustomer($name, $email)
+{
+    $dbconn3 = pg_connect("host=db.ecs.vuw.ac.nz dbname=mapleyhayl_jdbc user=mapleyhayl password=pass123") or die("Can't connect to database" . pg_last_error());
     
-    $sql = pg_query($dbconn3,"INSERT INTO customers (name,email) VALUES('$name', '$email')");
+    $sql = pg_query($dbconn3, "INSERT INTO customers (name,email) VALUES('$name', '$email')");
     
     $query = "SELECT customer_ID FROM customers where name = '$name' AND email = '$email'";
     $result = pg_query($query);
-    if (!$result) {
+    if (! $result) {
         echo "Problem with query " . $query . "<br/>";
         echo pg_last_error();
         exit();
@@ -22,36 +24,65 @@ function createCustomer($name, $email) {
     $customerID = $row['customer_id'];
     $timestamp = date('Y-m-d G:i:s');
     
-    echo "create customer method here";
-    
     createOrder($customerID, $timestamp);
-    
 }
 
 if (isset($_POST['confirm'])) {
     createCustomer($name, $email);
 }
 
-
-
-
-function createOrder($customerID, $timestamp) {
+function createOrder($customerID, $timestamp)
+{
+    $dbconn3 = pg_connect("host=db.ecs.vuw.ac.nz dbname=mapleyhayl_jdbc user=mapleyhayl password=pass123") or die("Can't connect to database" . pg_last_error());
     
-    $dbconn3 = pg_connect("host=db.ecs.vuw.ac.nz dbname=mapleyhayl_jdbc user=mapleyhayl password=pass123")
-    or die("Can't connect to database".pg_last_error());
-    
-    $sql = pg_query($dbconn3,"INSERT INTO orders (customer_id,completed,timestamp) VALUES('$customerID', false, '$timestamp')");
+    $sql = pg_query($dbconn3, "INSERT INTO orders (customer_id,completed,timestamp) VALUES('$customerID', false, '$timestamp')");
     
     $query = "SELECT order_id FROM orders WHERE customer_id = '$customerID' AND timestamp='$timestamp'";
+    
     $result = pg_query($query);
-    if (!$result) {
+    
+    if (! $result) {
         echo "Problem with query " . $query . "<br/>";
         echo pg_last_error();
         exit();
     }
     $row = pg_fetch_assoc($result);
     $orderID = $row['order_id'];
+    
+    createBurger($orderID);
 }
+
+function createBurger($orderID){
+    
+    $bunLettuce = $_POST['Bun_Lettuce'];
+    $bunStandard = $_POST['Bun_Standard'];
+    $vegeLettuce = $_POST['Vege_Lettuce'];
+    $vegeTomato = $_POST['Vege_Tomato'];
+    $vegeOnion = $_POST['Vege_Onion'];
+    $vegePickles = $_POST['Vege_Pickles'];
+    $vegeBeetroot = $_POST['Vege_Beetroot'];
+    $cheeseCheddar = $_POST['Cheese_Cheddar'];
+    $cheeseVegan = $_POST['Cheese_Vegan'];
+    $pattyBeef = $_POST['Patty_Beef'];
+    $pattyChicken = $_POST['Patty_Chicken'];
+    $pattyTofu = $_POST['Patty_Tofu'];
+    $sauceTomato = $_POST['Sauce_Tomato'];
+    $sauceChilli = $_POST['Sauce_Chilli'];
+    $sauceAioli = $_POST['Sauce_Aioli'];
+    
+    $dbconn3 = pg_connect("host=db.ecs.vuw.ac.nz dbname=mapleyhayl_jdbc user=mapleyhayl password=pass123") 
+    or die("Can't connect to database" . pg_last_error());
+    
+    $sql = pg_query($dbconn3, "INSERT INTO burger_ingredients (order_id, bun_lettuce, bun_standard, vege_lettuce, vege_tomato, vege_onion, vege_pickles, vege_beetroot, cheese_cheddar, cheese_vegan, patty_beef, patty_chicken, patty_tofu, sauce_tomato, sauce_chilli, sauce_aioli)
+           VALUES('$orderID', '$bunLettuce', '$bunStandard', '$vegeLettuce', '$vegeTomato', '$vegeOnion', '$vegePickles', '$vegeBeetroot', '$cheeseCheddar', '$cheeseVegan', '$pattyBeef', '$pattyChicken', '$pattyTofu', '$sauceTomato', '$sauceChilli', '$sauceAioli')");
+
+    
+    
+}
+    
+    
+     
+
 
 // header('Location: testform2.php');
 
--- BurgerProduction/WebApp/testform2.php
+++ BurgerProduction/WebApp/testform2.php
@@ -25,40 +25,42 @@ print_r($value);
 
 
 
-<p>Total Cost: $<span id="total"></span></p>
-<output id = "total1" name=total> test</output><br>
-
-Lettuce Bun: <input type="checkbox" id="Bun_Lettuce"><br>
-
-Standard bun: <input type="checkbox" id="Bun_Standard"><br>
-
-veggie lettuce: <input type="number" id="Vege_Lettuce"><br>
-
-Vege tomato: <input type="number" id="Vege_Tomato"><br>
-
-vege onion: <input type="number" id="Vege_Onion"><br>
-
-vege pickles: <input type="number" id="Vege_Pickles"><br>
-
-vege beetroot: <input type="number" id="Vege_Beetroot"><br>
-
-cheese cheddar: <input type="number" id="Cheese_Cheddar"><br>
-
-cheese vegan: <input type="number" id="Cheese_Vegan"><br>
-
-patty beef: <input type="number" id="Patty_Beef"><br>
-
-patty chicken: <input type="number" id="Patty_Chicken"><br>
-
-patty tofu: <input type="number" id="Patty_Tofu"><br>
-
-sauce tomato: <input type="number" id="Sauce_Tomato"><br>
-
-sauce chilli: <input type="number" id="Sauce_Chilli"><br>
-
-Sauce aioli: <input type="number" id="Sauce_Aioli"><br>
+<p>Total Cost: $<span name="total"></span></p>
+<output name = "total1" name=total> test</output><br>
 
 <form action="orderConfirm.php" method="post">
+
+Lettuce Bun: <input type="number" name="Bun_Lettuce" min="0" max="1"><br>
+
+Standard bun: <input type="number" name="Bun_Standard" min="0" max="1"><br>
+
+veggie lettuce: <input type="number" name="Vege_Lettuce"><br>
+
+Vege tomato: <input type="number" name="Vege_Tomato"><br>
+
+vege onion: <input type="number" name="Vege_Onion"><br>
+
+vege pickles: <input type="number" name="Vege_Pickles"><br>
+
+vege beetroot: <input type="number" name="Vege_Beetroot"><br>
+
+cheese cheddar: <input type="number" name="Cheese_Cheddar"><br>
+
+cheese vegan: <input type="number" name="Cheese_Vegan"><br>
+
+patty beef: <input type="number" name="Patty_Beef"><br>
+
+patty chicken: <input type="number" name="Patty_Chicken"><br>
+
+patty tofu: <input type="number" name="Patty_Tofu"><br>
+
+sauce tomato: <input type="number" name="Sauce_Tomato"><br>
+
+sauce chilli: <input type="number" name="Sauce_Chilli"><br>
+
+Sauce aioli: <input type="number" name="Sauce_Aioli"><br>
+
+
 <input type="text" name="name" placeholder="Name" required></input>
 <input type="text" name="email" placeholder="Email" required></input>
 <button name ="confirm" type="submit">Confirm</button>
@@ -67,54 +69,10 @@ Sauce aioli: <input type="number" id="Sauce_Aioli"><br>
 
 <output id= "total2" >test</output>
 
-
-
 <script type="text/javascript">
 
-
-$total = "1";
-
-document.getElementById("total1").innerHTML = 5 + 6;
-
-
 var ingredientsArray = <?php echo json_encode($arr);?>;
-var i = 0;
 
-//for(i;i<ingredientsArray.length;i++){
-//	var current =  ingredientsArray[i];
-//	var name = current.name;
-//	var quantity = current.quantity;
-//	alert(name.toString()+ " "+ quantity.toString());
-//}
-
-function myFunction(element) {
-	  var id = element;
-	  // Get the number
-//	  var number = document.getElementById("Bun_Lettuce");
-	  //var number = document.getElementById(id);
-	  var quantity = document.getElementById(id);
-	  // Get the output text
-//	  var text = document.getElementById("Bun_Lettuce1");
-	  var text = document.getElementById(id + "1");
-	  
-	  var total = document.getElementById("total1");
-
-	  // If the number is checked, display the output text
-// 	  if (number.checked == true){
-// 	    text.style.display = "block";
-// 	    total.innerHTML = 1;
-// 	  } else {
-// 	    text.style.display = "none";
-// 	  }
-
-		//Deal with input type "number"<input type="number" name="quantity" min="1" max="5">
-// 	  if (quantity > 0){
-// 		    text.style.display = "block";
-// 		    total.innerHTML = 1;
-// 		  } else {
-// 		    text.style.display = "none";
-// 		  }
-	}
 </script>
 
 
