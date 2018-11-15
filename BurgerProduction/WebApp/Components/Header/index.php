be6e972077708298f64589071b88367574767aca
hayleymapley <hmapley@gmail.com>
2018-11-16 12:53:34 +1300

    Removed WebApp folder (copy on my desktop if needed)

--- BurgerProduction/WebApp/Components/Header/index.php
+++
@@ -1,6 +0,0 @@
-<div class="menubar">
-    <a href="ContactUs.php">Contact Us</a>
-    <a href="Pricing.php">Pricing</a>
-    <a href="OurPeople.html">Our People</a>
-    <a href="index.html">Home</a>
-</div>
